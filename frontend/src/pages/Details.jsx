d5b29409f936615b12b1a20a5aea313e6072a823
Kauan <kauan.s.leite@hotmail.com>
2023-03-23 11:40:16 -0300

    feat: details product completed

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -2,10 +2,12 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import Menu from '../components/Menu'
+import SkeletonLoadingDetails from '../components/SkeletonLoadingDetails'
 import { getComicsByID } from '../redux/actions'
+import '../style/Details.css'
 
 function Details (props) {
-  const { dispatch, products } = props
+  const { dispatch, products, isFetching } = props
   const { id } = props.match.params
 
   useEffect(() => {
@@ -15,24 +17,48 @@ function Details (props) {
   return (
     <div>
       <Menu />
-      { products.map((product) => {
-        return (
-          <div key={product.id}>
-            <h1>{product.id}</h1>
-            <h1>{product.title}</h1>
-            <h1>{product.isbn}</h1>
-            <h1>{product.pageCount}</h1>
-            <h1>{product.prices[0].price}</h1>
-            <img src={`${product.thumbnail.path}.${product.thumbnail.extension}`} alt={`capa do ${product.title}`}/>
-            {product.creators.items.map((author) => {
-              return (
-                <h1 key={author.name}>{author.name}</h1>
-              )
-            })}
-          </div>
-        )
-      })}
+      {
+        isFetching
+          ? <SkeletonLoadingDetails />
+          : products.map((product) => {
+            return (
+          <section key={product.id}>
+            <div className='left'>
+              <img className='thumb-details' src={`${product.thumbnail.path}.${product.thumbnail.extension}`} alt={`capa do ${product.title}`}/>
+            </div>
+            <div className='right'>
+              <h1 className='title-details'>{product.title}</h1>
+              { product.isbn.length > 0 ? <h1 className='subtext'>ISBN: {product.isbn}</h1> : <h1 className='subtext'>ISBN: Desconhecido</h1>}
+              { product.pageCount > 0 ? <h1 className='subtext'>Número de Páginas: {product.pageCount}</h1> : <h1 className='subtext'>Número de Páginas: Desconhecido</h1>}
+              <div className='authors'>
+              <h1>Autores</h1>
+                <div className='authors-name'>
+                  { product.creators.items.length > 0
+                    ? product.creators.items.map((author) => {
+                      return (
+                      <h1 className='subtext' key={author.name}>{author.name}</h1>
+                      )
+                    })
+                    : <h1 className='subtext'>Desconhecido</h1>}
+                </div>
+              </div>
+              {
+                product.prices[0].price !== 0
+                  ? <div>
+                      <h1 className='pcs-details'>R$ {product.prices[0].price.toFixed(2)}</h1>
+                      <button className='add-cart-details'>Add To Cart</button>
+                    </div>
+                  : <div>
+                      <h1 className='out'>Out of Stock</h1>
+                    </div>
+              }
+            </div>
+          </section>
+            )
+          })
+    }
     </div>
+
   )
 }
 
